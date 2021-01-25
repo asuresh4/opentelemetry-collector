a8bebea67855111ea58956d55b4944cfc3439238
Akash Suresh <akash@signalfx.com>
2021-01-25 17:04:52 -0500

    hostmetricsreceiver (network): Refactor to use metric metadata

--- receiver/hostmetricsreceiver/internal/scraper/networkscraper/network_metadata.go
+++ receiver/hostmetricsreceiver/internal/scraper/networkscraper/network_metadata.go
@@ -14,90 +14,86 @@
 
 package networkscraper
 
-import (
-	"go.opentelemetry.io/collector/consumer/pdata"
-)
-
-// labels
-
-const (
-	deviceLabelName    = "device"
-	directionLabelName = "direction"
-	protocolLabelName  = "protocol"
-	stateLabelName     = "state"
-)
-
-// direction label values
-
-const (
-	receiveDirectionLabelValue  = "receive"
-	transmitDirectionLabelValue = "transmit"
-)
-
-// protocol label values
-
-const (
-	tcpProtocolLabelValue = "tcp"
-)
-
-// descriptors
-
-var networkPacketsDescriptor = func() pdata.Metric {
-	metric := pdata.NewMetric()
-	metric.SetName("system.network.packets")
-	metric.SetDescription("The number of packets transferred.")
-	metric.SetUnit("{packets}")
-	metric.SetDataType(pdata.MetricDataTypeIntSum)
-	sum := metric.IntSum()
-	sum.SetIsMonotonic(true)
-	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
-	return metric
-}()
-
-var networkDroppedPacketsDescriptor = func() pdata.Metric {
-	metric := pdata.NewMetric()
-	metric.SetName("system.network.dropped")
-	metric.SetDescription("The number of packets dropped.")
-	metric.SetUnit("{packets}")
-	metric.SetDataType(pdata.MetricDataTypeIntSum)
-	sum := metric.IntSum()
-	sum.SetIsMonotonic(true)
-	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
-	return metric
-}()
-
-var networkErrorsDescriptor = func() pdata.Metric {
-	metric := pdata.NewMetric()
-	metric.SetName("system.network.errors")
-	metric.SetDescription("The number of errors encountered")
-	metric.SetUnit("{errors}")
-	metric.SetDataType(pdata.MetricDataTypeIntSum)
-	sum := metric.IntSum()
-	sum.SetIsMonotonic(true)
-	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
-	return metric
-}()
-
-var networkIODescriptor = func() pdata.Metric {
-	metric := pdata.NewMetric()
-	metric.SetName("system.network.io")
-	metric.SetDescription("The number of bytes transmitted and received")
-	metric.SetUnit("By")
-	metric.SetDataType(pdata.MetricDataTypeIntSum)
-	sum := metric.IntSum()
-	sum.SetIsMonotonic(true)
-	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
-	return metric
-}()
-
-var networkConnectionsDescriptor = func() pdata.Metric {
-	metric := pdata.NewMetric()
-	metric.SetName("system.network.connections")
-	metric.SetDescription("The number of connections")
-	metric.SetUnit("{connections}")
-	metric.SetDataType(pdata.MetricDataTypeIntSum)
-	sum := metric.IntSum()
-	sum.SetIsMonotonic(false)
-	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
-	return metric
-}()
+//// labels
+//
+//const (
+//	deviceLabelName    = "device"
+//	directionLabelName = "direction"
+//	protocolLabelName  = "protocol"
+//	stateLabelName     = "state"
+//)
+//
+//// direction label values
+//
+//const (
+//	receiveDirectionLabelValue  = "receive"
+//	transmitDirectionLabelValue = "transmit"
+//)
+//
+//// protocol label values
+//
+//const (
+//	tcpProtocolLabelValue = "tcp"
+//)
+//
+//// descriptors
+//
+//var networkPacketsDescriptor = func() pdata.Metric {
+//	metric := pdata.NewMetric()
+//	metric.SetName("system.network.packets")
+//	metric.SetDescription("The number of packets transferred.")
+//	metric.SetUnit("{packets}")
+//	metric.SetDataType(pdata.MetricDataTypeIntSum)
+//	sum := metric.IntSum()
+//	sum.SetIsMonotonic(true)
+//	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+//	return metric
+//}()
+//
+//var networkDroppedPacketsDescriptor = func() pdata.Metric {
+//	metric := pdata.NewMetric()
+//	metric.SetName("system.network.dropped")
+//	metric.SetDescription("The number of packets dropped.")
+//	metric.SetUnit("{packets}")
+//	metric.SetDataType(pdata.MetricDataTypeIntSum)
+//	sum := metric.IntSum()
+//	sum.SetIsMonotonic(true)
+//	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+//	return metric
+//}()
+//
+//var networkErrorsDescriptor = func() pdata.Metric {
+//	metric := pdata.NewMetric()
+//	metric.SetName("system.network.errors")
+//	metric.SetDescription("The number of errors encountered")
+//	metric.SetUnit("{errors}")
+//	metric.SetDataType(pdata.MetricDataTypeIntSum)
+//	sum := metric.IntSum()
+//	sum.SetIsMonotonic(true)
+//	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+//	return metric
+//}()
+//
+//var networkIODescriptor = func() pdata.Metric {
+//	metric := pdata.NewMetric()
+//	metric.SetName("system.network.io")
+//	metric.SetDescription("The number of bytes transmitted and received")
+//	metric.SetUnit("By")
+//	metric.SetDataType(pdata.MetricDataTypeIntSum)
+//	sum := metric.IntSum()
+//	sum.SetIsMonotonic(true)
+//	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+//	return metric
+//}()
+//
+//var networkConnectionsDescriptor = func() pdata.Metric {
+//	metric := pdata.NewMetric()
+//	metric.SetName("system.network.connections")
+//	metric.SetDescription("The number of connections")
+//	metric.SetUnit("{connections}")
+//	metric.SetDataType(pdata.MetricDataTypeIntSum)
+//	sum := metric.IntSum()
+//	sum.SetIsMonotonic(false)
+//	sum.SetAggregationTemporality(pdata.AggregationTemporalityCumulative)
+//	return metric
+//}()
